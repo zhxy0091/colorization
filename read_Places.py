cc69e83d02ffdf49ac71e5c7ed9b6a5ac454bbda
Xinyuan Zhang <zhangxinyuan1995@gmail.com>
2018-03-29 15:09:38 -0400

    add train file

--- read_Places.py
+++ read_Places.py
@@ -1,23 +1,18 @@
 import glob
 import numpy as np
-# from PIL import Image
-import tensorflow as tf
 import math
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import glob
 
-
-
-
-def load_data():
+def read_dataset(data_dir, testing_percentage=0.0, validation_percentage=0.2):
     input_image = []
     i = 0
-    for filename in glob.iglob('../val_256/*',recursive=True):
+    for filename in glob.iglob(data_dir+'/*',recursive=True):
         #print("file_name", filename) 
         img = mpimg.imread(filename)
         if(img.shape == (256,256,3)):
-            input_image.append(list(mpimg.imread(filename)))
+            input_image.append(filename)
         else:
             print("file_name", filename, "is invalid") 
         if(i%10000 == 0):
@@ -26,16 +21,19 @@ def load_data():
             break
 
         i+=1
-    train_percentage = 0.8
-    input_image = np.asarray(input_image)
-    print(input_image.shape)
-    rand_indices = np.arange(input_image.shape[0])
-    np.random.shuffle(rand_indices)
-    lenn = int(train_percentage*input_image.shape[0])
-    data_train = input_image[rand_indices[0:lenn]]
-    data_test = input_image[rand_indices[lenn:]]
+    
+    np.random.shuffle(input_image)
+    no_of_images = len(input_image)
+    training_images = input_image
 
+    validation_offset = int(validation_percentage * no_of_images)
+    validation_images = training_images[:validation_offset]
+    test_offset = int(testing_percentage * no_of_images)
+    testing_images = training_images[validation_offset:validation_offset + test_offset]
+    training_images = training_images[validation_offset + test_offset:]
 
-    print("Shape of data_train, data_test:", data_train.shape, data_test.shape)
-    return data_train, data_test
+    print ("Training: %d, Validation: %d, Test: %d" % (
+        len(training_images), len(validation_images), len(testing_images)))
+    return training_images, testing_images, validation_images
+
 
